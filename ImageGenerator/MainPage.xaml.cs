33b73563284516583bbdf1036878a3de706e4f99
spiderlinus <linusschon08@gmail.com>
2025-10-15 14:00:50 +0200

    Deleted NotifyMethods

--- ImageGenerator/MainPage.xaml.cs
+++ ImageGenerator/MainPage.xaml.cs
@@ -81,11 +81,5 @@ namespace ImageGenerator
                 };
             }
         }
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //protected void OnPropertyChanged([CallerMemberName] string name = null)
-        //    => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-
     }
 }
